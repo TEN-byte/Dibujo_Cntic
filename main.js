a3c6fadbcdfe76bd429ca374e73add7c3eee97cb
TEN-byte <byte.ten@yahoo.com>
2021-01-02 22:13:43 -0600

    newUpdate

--- main.js
+++ main.js
@@ -1,26 +1,91 @@
-document.getElementById("text").innerHTML = "Incrementa el slider y da click para cambiar el color";
+document.getElementById("text").innerHTML = "Click sostenido y dibuja";
+
+let paths = [];
+let painting = false;
+let next = 0;
+
+let current;
+let previous;
 
 function setup(){
-createCanvas(1280, 700, WEBGL);
-detailX = createSlider(3, 24, 3);
-detailX.position(10, 20);
-detailX.style('width', '80px');
+  createCanvas(1280, 720);
+  current = createVector(0, 0);
+  previous = createVector(0, 0);
+}
+function draw(){
+  background(100, 200, 190);
+
+  if (millis() > next && painting){
+    current.x = mouseX;
+    current.y = mouseY;
+    
+    let force = p5.Vector.sub(current, previous);
+    force.mult(0.05);
+    paths[paths.length - 1].add(current, force);
+
+    next = millis() + random(100);
+
+    previous.x = current.x;
+    previous.y = current.y;
+  }
+    for (let i = 0; i < paths.length; i++){
+      paths[i].update();
+      paths[i].display();
+
+    }
+}
+function mousePressed(){
+  next = 0;
+  painting = true;
+  previous.x = mouseX;
+  previous.y = mouseY;
+  paths.push(new Path());
+}
+function mouseReleased(){
+  painting = false;
+}
+class Path {
+  constructor(){
+    this.particles = [];
+    this.hue = random(100);
+  }
+  add(position, force){
+    this.particles.push(new Particle(position, force, this.hue));
+  }
+  update(){
+    for(let i = 0; i < this.particles.length; i++)
+    this.particles[i].update();
+  }
+  display(){
+    for (let i = this.particles.length - 1; i >= 0; i--){
+      if (this.particles[i].lifespan <= 0){
+        this.particles.splice(i, 1);
+      } else {
+        this.particles[i].display(this.particles[i + 1]);
+      }
+    }
+  }
 }
 
-let preset = 0;
+class Particle{
+  constructor(position, force, hue){
+    this.position = createVector(position.x, position.y);
+    this.velocity = createVector(force.x, force.y);
+    this.drag = 0.95;
+    this.lifespan = 255;
+  }
+  update(){
+    this.position.add(this.velocity);
+    this.velocity.mult(this.drag);
+    this.lifespan--;
+  }
+  display(other){
+    stroke(0, this.lifespan);
+    fill(0, this.lifespan/2);
+    ellipse(this.position.x, this.position.y, 8, 8);
 
- function draw(){
-   background(100, 200, 190);
-   fill(preset);
-   orbitControl();
-   rotateX(millis()/1000);
-   rotateY(millis()/1000);
-   torus(100, 90, detailX.value(), 12);
+    if(other){
+      line(this.position.x, this.position.y, other.position.x, other.position.y);
+    }
+  }
 }
- function mouseClicked(){
-   if (preset === 0){
-     preset = random(255);
-   } else{
-     preset = random(255);
-   }
- }
